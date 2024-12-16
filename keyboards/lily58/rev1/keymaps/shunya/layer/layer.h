5de9dfc7f8fa5e132cb7b198b545aba5ebcde261
Shunya <luque.viictor@gmail.com>
2024-12-16 22:52:12 +0100

    Modularize keymap.c

---
+++ keyboards/lily58/rev1/keymaps/shunya/layer/layer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/* Each layer gets a name for readability.        */
+/* The underscores don't mean anything - you can  */
+/* have a layer called STUFF or any other name.   */
+/* Layer names don't all need to be of the same   */
+/* length, and you can also skip them entirely    */
+/* and just use numbers.                          */
+
+enum layer_number {
+  _QWERTY = 0,
+  _FUNC,
+  _SYMB,
+  _ADJUST,
+};
