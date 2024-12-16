214035f89d880f9dd0b9bbe8704373d8eee3d397
Shunya <luque.viictor@gmail.com>
2024-12-16 22:52:35 +0100

    Add oled module

---
+++ keyboards/lily58/rev1/keymaps/shunya/oled/oled.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef OLED_ENABLE
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation);
+void render_logo(void);
+bool oled_task_user(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+#endif /* OLED_ENABLE */
